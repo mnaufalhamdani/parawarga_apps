f3bcc3ed4ee7835eb0f67aa067f03059d685fefa
dani <dani@duakelinci.co.id>
2024-12-20 10:18:23 +0700

    create standard error page

---
+++ lib/theme/standard_error_page.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, use_super_parameters, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class StandardErrorPage extends StatefulWidget {
+  final String? message;
+  final void Function()? onPressed;
+
+  StandardErrorPage({
+    Key? key,
+    this.message,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  State<StandardErrorPage> createState() => StandardErrorPageState();
+}
+
+class StandardErrorPageState extends State<StandardErrorPage> {
+  var obscurePassword = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(padding: EdgeInsets.only(top: 100), child: Center(
+        child: GestureDetector(
+            onTap: () {
+              if(widget.onPressed != null){
+                widget.onPressed!();
+              }
+            },
+            child: SingleChildScrollView(child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image(
+                    height: 200,
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                        "assets/images/background/gif_no_data.gif")),
+                Text(
+                  widget.message.toString(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black87),
+                )
+              ],
+            ))
+        ))
+    );
+  }
+}
